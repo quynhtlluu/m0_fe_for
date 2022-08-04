253e8afcfba172735cdfe529291f9255642c7ad4
Quynh Luu <quynhtlluu@gmail.com>
2022-08-03 19:36:59 -0700

    Add answers to Challnge 2 and Challenge 3

--- for.js
+++ for.js
@@ -12,11 +12,21 @@ for (var i = 25; i <= 75; i++) {
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+for (var i = 0; i <= weather_options.length; i++) {
+  if (weather_options[i]) {
+    console.log(`The weather is ${weather_options[i]} today.`);
+    }
+}
+// This will list five sentences stating each type of weather. Not sure if this is what you all wanted.
 
-
-// Challenge 3
+// Challenge 3s
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
-var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"];
+
+for (var i = 0; i < words.length; i++) {
+    if (words[i].length === 3)
+        console.log(words[i])
+}
 
 // 📹 Record yourself live-coding and talking through any ONE of the challenges.
   // The recording doesn't have to be the first time you are solving the problem.
